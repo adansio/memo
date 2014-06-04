721ab1b843e385d9b2e96019c6d588010eec4e7f
adansio <adansio@gmail.com>
2014-06-04 17:41:04 -0400

    recorrer en espiral, calcular Prx, y mapa calor. estatico 1 ap

--- test.m
+++ test.m
@@ -1,85 +1,89 @@
 function test
 
-global matriz step current limit posx posy vary varx
+global matriz step current limit posx posy vary varx Ptx
 
-matriz = nan(10, 10);
+matriz = nan(100, 100);
 current = 0;
-limit = 5;
-posx = 5;
-posy = 5;
-varx = 5;
-vary = 5;
+limit = 70;
+posx = 50;
+posy = 50;
+varx = 50;
+vary = 50;
+Ptx = 15; %  PTx en dBm
 
 matriz = llenar(matriz);
-matriz = espiral(matriz, current, limit, step, posy, posx, vary, varx);
+matriz = espiral(matriz, current, limit, posy, posx, vary, varx, Ptx);
 matriz
+colormap('default')
+imagesc(matriz)
+colorbar
 
 end
 
-function mtr = llenar(mtr)
+function mtr = llenar(mtr)  %llena matriz con 0's
     
-    mtr(1:10,1:10)=0;
+    mtr(1:100,1:100)=0;
 
 end
 
-function mtr = espiral(mtr, current, limit, step, px, py, vy, vx)
+function mtr = espiral(mtr, current, limit, px, py, vy, vx, Pt)
     
     while current<limit 
         current=current+1;
         step=0;
         while step<current 
             step=step+1;
-            [mtr, vx]=right(mtr, vx, vy, step);
+            [mtr, vx]=right(mtr, vx, vy, px, py, Pt);
         end
         
         step=0;
         while step<current
             step=step+1;
-            [mtr, vy]=down(mtr, vx, vy, step);
+            [mtr, vy]=down(mtr, vx, vy, px, py, Pt);
         end
         
         current=current+1;
         step=0;
         while step<current
             step=step+1;
-            [mtr, vx] = left(mtr, vx, vy, step);
+            [mtr, vx] = left(mtr, vx, vy, px, py, Pt);
         end
         
         step=0;
         while step<current
             step=step+1;
-            [mtr, vy] = up(mtr, vx, vy, step);
+            [mtr, vy] = up(mtr, vx, vy, px, py, Pt);
         end
         
     end
 
 end
 
-function [mtr, vx] = right(mtr, vx, vy, step)
+function [mtr, vx] = right(mtr, vx, vy, px, py, Pt)
     
     vx=vx+1;
-    mtr(vy,vx) = step;
+    mtr(vy,vx) = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
 
 end
 
-function [mtr, vy] = down(mtr, vx, vy, step)
+function [mtr, vy] = down(mtr, vx, vy, px, py, Pt)
 
     vy=vy+1;
-    mtr(vy,vx) = step;
+    mtr(vy,vx) = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
     
 end
 
-function [mtr, vx] = left(mtr, vx, vy, step)
+function [mtr, vx] = left(mtr, vx, vy, px, py, Pt)
 
     vx=vx-1;
-    mtr(vy,vx) = step;
+    mtr(vy,vx) = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
     
 end
 
-function [mtr, vy] = up(mtr, vx, vy, step)
+function [mtr, vy] = up(mtr, vx, vy, px, py, Pt)
 
     vy=vy-1;
-    mtr(vy,vx) = step;
+    mtr(vy,vx) = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
     
 end
 
