961ce3e8b57d3d91e5c7093dfd447e336e74e5ca
adansio <adansio@gmail.com>
2014-06-17 20:04:56 -0400

    avanzar en linea v2

--- linea.c
+++ linea.c
@@ -3,20 +3,20 @@
 #include<math.h>
 
 int matriz[20][20];
-int posx1=2,posx2=11,posy1=2,posy2=17;
+int apx=5,apy=5,ptox=10,ptoy=5;
 
 void llenar() {
 	int x,y;
 
-	for(x=0;x<20;x++)
-		for(y=0;y<20;y++)
+	for(y=0;y<20;y++)
+		for(x=0;x<20;x++)
 			matriz[x][y]=0;
 }
 
 void mostrar() {
 	int x,y;
-	for(x=0;x<10;x++){
-		for(y=0;y<10;y++)
+	for(y=0;y<20;y++){
+		for(x=0;x<20;x++)
 			printf("%d ", matriz[x][y]);
 		printf("\n");
 	}
@@ -25,44 +25,65 @@ void mostrar() {
 void linea () {
 	int i;
 	float p1,p2,p3,p4;	//p1 der, p2 abajo, p3 izq, p4 arriba
-
-	if ( p1 = sqrt(abs(pow(posx2+1-posx1,2)) + abs(pow(posy2-posy1,2))) < 
-			p2 = sqrt(abs(pow(posx2-posx1,2)) + abs(pow(posy2+1-posy1,2))) )
-		if ( p1 < p3 = sqrt(abs(pow(posx2-1-posx1,2)) + abs(pow(posy2-posy1,2))) )
-			if ( p1 < p4 = sqrt(abs(pow(posx2-posx1,2)) + abs(pow(posy2-1-posy1,2))) ) {
-				posx1++;
-				matriz[posx1][posy1]=1;
-			}
-			else {
-				posy1++;
-				matriz[posx1][posy1]=1; 
-			}
-		
-
-
-
-
-
 	
+	// caso 0 pto se encuentra en el mismo que el AP, exit(0)
+	if ( (apx-ptox) == 0 && (apy-ptoy) == 0)	{
+		printf("pared en mismo lugar que el AP \n");
+		exit(0);
+	}
+	
+	// caso 1-2 pendiente infinita -> x1 == x2
+	if ( (apx-ptox) == 0 )
+		if ( ptoy > apy ) {
+			ptoy--;
+			matriz[ptox][ptoy]=1;
+		}
+		else {
+			ptoy++;
+			matriz[ptox][ptoy]=1;
+		}
+	
+	//caso 3-4 pendiente 0 -> y1 == y2
+	if ( (apy-ptoy) == 0 )
+		if ( ptox > apx ) {
+			ptox--;
+			matriz[ptox][ptoy]=1;
+		}
+		else {
+			ptox++;
+			matriz[ptox][ptoy]=1;
+		}
+	//caso 5-6 pendiente >= 1+ >= 1-
+	if ( ((ptoy-apy)/(ptox-apx)) >= 1 )
+		if ( (ptoy<apy) && (ptox<apx) ) {
 
-	//derecha
-	p1 = sqrt(abs(pow(posx2+1-posx1,2)) + abs(pow(posy2-posy1,2)));
-	//abajo
-	p2 = sqrt(abs(pow(posx2-posx1,2)) + abs(pow(posy2+1-posy1,2)));
-	//izq
-	p3 = sqrt(abs(pow(posx2-1-posx1,2)) + abs(pow(posy2-posy1,2)));
-	//arriba
-	p4 = sqrt(abs(pow(posx2-posx1,2)) + abs(pow(posy2-1-posy1,2)));
+		}
+		else {
 
+		}
 
+	//caso 7-8 pendiente <= -1+ <= -1-
+	if ( ((ptoy-apy)/(ptox-apx)) < -1 )
+		if ( (apy<ptoy) && (ptox<apx) ) {
+
+		}
+		else {
+
+		}
+	
+	//caso 9-10 pendiente ]0,1]
+	if ( ((ptoy-apy)/(pto
+
+}
 
 
 void main(){
 
 	llenar();
-	matriz[posx1][posy1]=1;
-	matriz[posx2][posy2]=1;
+	matriz[apx][apy]=1;
+	matriz[ptox][ptoy]=1;
 
 	linea();
+	mostrar();
 
 }
