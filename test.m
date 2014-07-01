9459e6b6fead0ee322b878764ae3a70e5330ea26
adansio <adansio@gmail.com>
2014-06-30 20:27:04 -0400

    corrigiendo restricciones

--- test.m
+++ test.m
@@ -14,11 +14,14 @@
 
 function test
 
-    global mapa_LOS mapa_NLOS current limit apx apy vary varx Ptx ch
+    global mapa_LOS mapa_NLOS current apx apy vary varx Ptx UPr ch
             %  apx posicion del ap en eje x
             %  apy posicion del ap en eje y
             %  varx posicion de analisis en eje x
             %  vary posicion de analisis en eje y
+            %  Ptx Potencia de transmision
+            %  UPr Umbral de potencia de recepcion minimo [dBm]
+            %  ch  channel
 
     dim1 = 1000;     %dimensiones del mapa
     dim2 = 1000;
@@ -26,22 +29,25 @@ function test
     mapa_LOS = nan(dim1, dim2);         %   mapa con aps con linea vista   escala => 1 [pto en matriz] : 1 [m]
     mapa_NLOS = nan(dim1, dim2);        %   mapa con paredes y obstrucciones no linea vista
     current = 0;    
-    limit = 370;                         %   vuelta limite - Prx minima
+    %limit = 370;                            vuelta limite - Prx minima
     APs = [                             %   eventuales ptos con aps, y sus características
-        250 250 15 1;                     %   x y Ptx[dBm] ch 
+        250 250 15 1;                   %   x y Ptx[dBm] ch 
         400 250 12 6; 
-        600 250 6 11];     
+        600 250 6 11
+        120 600 9 11
+        500 600 12 6];     
     
-    %for i=1:220
+    %for i=1:800
     %    mapa_NLOS(90, i) = 5;
     %end
     for i=100:800
         mapa_NLOS(i, 100) = 5;
     end
-        
+    
+    UPr = -71;                               
     mapa_LOS = llenar(mapa_LOS);
     
-    for i=1:3
+    for i=1:size(APs,1)
         apx = APs(i,1);
         apy = APs(i,2);
         Ptx = APs(i,3);
@@ -50,7 +56,7 @@ function test
         varx = apx;
         vary = apy;
     
-        mapa_LOS = espiral(mapa_NLOS, mapa_LOS, current, limit, apx, apy, vary, varx, Ptx);
+        mapa_LOS = espiral(mapa_NLOS, mapa_LOS, current, apx, apy, vary, varx, Ptx, UPr);
     end
 
     % mapa_LOS
@@ -63,38 +69,49 @@ end
 %llena mapa_LOS con ruido ambiente -90 dB
 function mtr = llenar(mtr)  
     
-    mtr(1:700,1:700)=-100;
+    mtr(1:1000,1:1000)=-100;
 
 end
 
 %avance en espiral
-function mtr = espiral(nlos, mtr, current, limit, px, py, vy, vx, Pt)
+function mtr = espiral(nlos, mtr, current, px, py, vy, vx, Pt, UPr)
     
-    while current<limit 
+    %variable booleana - rango maximo de cobertura para cierta potencia
+    aux1 = 1; aux2 = 1; aux3 = 1; aux4 = 1;
+    
+    
+    while aux1 || aux2 || aux3 || aux4 
+        
+        aux1 = 0; aux2 = 0; aux3 = 0; aux4 = 0;
+        %fprintf('wena \n');
         current=current+1;
         step=0;
         while step<current 
             step=step+1;
-            [mtr, vx]=right(nlos, mtr, vx, vy, px, py, Pt);
+            [mtr, vx, rm1]=right(nlos, mtr, vx, vy, px, py, Pt, UPr);
+            aux1 = aux1 || rm1;
         end
         
         step=0;
         while step<current
             step=step+1;
-            [mtr, vy]=down(nlos, mtr, vx, vy, px, py, Pt);
+            [mtr, vy, rm2]=down(nlos, mtr, vx, vy, px, py, Pt, UPr);
+            aux2 = aux2 || rm2;
         end
         
         current=current+1;
         step=0;
-         while step<current
+        while step<current
             step=step+1;
-            [mtr, vx] = left(nlos, mtr, vx, vy, px, py, Pt);
+            [mtr, vx, rm3] = left(nlos, mtr, vx, vy, px, py, Pt, UPr);
+            aux3 = aux3 || rm3;
         end
         
         step=0;
         while step<current
             step=step+1;
-            [mtr, vy] = up(nlos, mtr, vx, vy, px, py, Pt);
+            [mtr, vy, rm4] = up(nlos, mtr, vx, vy, px, py, Pt, UPr);
+            aux4 = aux4 || rm4;
         end
         
     end
@@ -102,53 +119,66 @@ function mtr = espiral(nlos, mtr, current, limit, px, py, vy, vx, Pt)
 end
 
 % avanzar a la derecha
-function [mtr, vx] = right(nlos, mtr, vx, vy, px, py, Pt)
+function [mtr, vx, rm1] = right(nlos, mtr, vx, vy, px, py, Pt, UPr)
     
     vx=vx+1;
     Prx = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
     atenuacion = linea(nlos, px, py, vx, vy);
     Prx = Prx - atenuacion;
-    if Prx > -70
+    if Prx > UPr
         mtr(vy,vx) = Prx;
-    end        
+        rm1 = true;
+    else
+        rm1 = false;
+    end 
+    
 
 end
 
 %avanzar hacia abajo
-function [mtr, vy] = down(nlos, mtr, vx, vy, px, py, Pt)
+function [mtr, vy, rm2] = down(nlos, mtr, vx, vy, px, py, Pt, UPr)
 
     vy=vy+1;
     Prx = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
     atenuacion = linea(nlos, px, py, vx, vy);
     Prx = Prx - atenuacion;
-    if Prx > -70
+    if Prx > UPr
         mtr(vy,vx) = Prx;
+        rm2 = true;
+    else
+        rm2 = false;
     end
     
 end
 
 %avanzar hacia la izquierda
-function [mtr, vx] = left(nlos, mtr, vx, vy, px, py, Pt)
+function [mtr, vx, rm3] = left(nlos, mtr, vx, vy, px, py, Pt, UPr)
 
     vx=vx-1;
     Prx = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
     atenuacion = linea(nlos, px, py, vx, vy);
     Prx = Prx - atenuacion;
-    if Prx > -70
+    if Prx > UPr
         mtr(vy,vx) = Prx;
+        rm3 = true;
+    else
+        rm3 = false;
     end
     
 end
 
 %avanzar hacia arriba
-function [mtr, vy] = up(nlos, mtr, vx, vy, px, py, Pt)
+function [mtr, vy, rm4] = up(nlos, mtr, vx, vy, px, py, Pt, UPr)
 
     vy=vy-1;
     Prx = Pt + 20 * log10(0.125/(4*pi*sqrt((vx-px)^2+(vy-py)^2)));
     atenuacion = linea(nlos, px, py, vx, vy);
     Prx = Prx - atenuacion;
-    if Prx > -70
+    if Prx > UPr
         mtr(vy,vx) = Prx;
+        rm4 = true;
+    else
+        rm4 = false;
     end
     
 end
